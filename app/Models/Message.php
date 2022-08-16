8415d8d443006684892ee4640df855585a395509
Iglebekk <anders@keystone.no>
2022-08-16 20:22:24 +0200

    Added relationships and fillable

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Message extends Model
-{
+class Message extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'receiver',
+        'code'
+    ];
+
+    public function account() {
+        return $this->belongsTo(Account::class);
+    }
 }
